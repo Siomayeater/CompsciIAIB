fe201fe6ffe8065cddc1bc3e56fd9489456390ec
JaydenWong123 <jaydenwongkar1832@gmail.com>
2024-10-10 14:38:43 +0700

    new changes again1

---
+++ compsci_ia/lib/pages/another_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AnotherPage extends StatelessWidget {
+  const AnotherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Another Page'),
+      ),
+      body: const Center(
+        child: Text('This is another page.'),
+      ),
+    );
+  }
+}
+lib/ui/settings_page.dart
+dart
+Copy code
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings Page'),
+      ),
+      body: const Center(
+        child: Text('This is the settings page.'),
+      ),
+    );
+  }
+}
