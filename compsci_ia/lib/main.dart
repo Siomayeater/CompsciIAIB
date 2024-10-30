1ef41b14893dda75a90266aec914b22f619f6456
JaydenWong123 <jaydenwongkar1832@gmail.com>
2024-10-30 13:09:34 +0700

    new one

--- compsci_ia/lib/main.dart
+++ compsci_ia/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart'; // Hive is used for local data storage
 import 'Login.dart';  // Importing the MyApp widget from my_app.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
   await _initHive();  // Initializes Hive (local database)
   runApp( const MyApp());  // Runs the MyApp widget
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 Future<void> _initHive() async {
